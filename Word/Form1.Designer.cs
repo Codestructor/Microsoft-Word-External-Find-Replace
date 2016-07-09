60ae7aad53f6f763f88251df766ca0cdea7c2823
Mihai Gherman <mihaita.gherman97@gmail.com>
2016-07-09 13:24:39 +0300

    Add files via upload

---
+++ Word/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Word
+{
+    partial class PrimeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.findTextTxtBox = new System.Windows.Forms.TextBox();
+            this.replaceTextTxtBox = new System.Windows.Forms.TextBox();
+            this.pathTxtBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.allRadioBtn = new System.Windows.Forms.RadioButton();
+            this.onlyRadioBtn = new System.Windows.Forms.RadioButton();
+            this.progressTxtBox = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(12, 351);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 1;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Insert text to be found and replaced:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Insert replacing text:";
+            // 
+            // findTextTxtBox
+            // 
+            this.findTextTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.findTextTxtBox.Location = new System.Drawing.Point(12, 76);
+            this.findTextTxtBox.Multiline = true;
+            this.findTextTxtBox.Name = "findTextTxtBox";
+            this.findTextTxtBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.findTextTxtBox.Size = new System.Drawing.Size(378, 55);
+            this.findTextTxtBox.TabIndex = 4;
+            this.findTextTxtBox.Text = "2015";
+            // 
+            // replaceTextTxtBox
+            // 
+            this.replaceTextTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.replaceTextTxtBox.Location = new System.Drawing.Point(12, 154);
+            this.replaceTextTxtBox.Multiline = true;
+            this.replaceTextTxtBox.Name = "replaceTextTxtBox";
+            this.replaceTextTxtBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.replaceTextTxtBox.Size = new System.Drawing.Size(378, 55);
+            this.replaceTextTxtBox.TabIndex = 5;
+            this.replaceTextTxtBox.Text = "2016";
+            // 
+            // pathTxtBox
+            // 
+            this.pathTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pathTxtBox.Location = new System.Drawing.Point(12, 29);
+            this.pathTxtBox.Multiline = true;
+            this.pathTxtBox.Name = "pathTxtBox";
+            this.pathTxtBox.Size = new System.Drawing.Size(378, 24);
+            this.pathTxtBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(252, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Insert directory/ specific file path:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(396, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Progress:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Replace in:";
+            // 
+            // allRadioBtn
+            // 
+            this.allRadioBtn.AutoSize = true;
+            this.allRadioBtn.Checked = true;
+            this.allRadioBtn.Location = new System.Drawing.Point(12, 232);
+            this.allRadioBtn.Name = "allRadioBtn";
+            this.allRadioBtn.Size = new System.Drawing.Size(269, 17);
+            this.allRadioBtn.TabIndex = 11;
+            this.allRadioBtn.TabStop = true;
+            this.allRadioBtn.Text = "All the files in the parent directory and subdirectories";
+            this.allRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // onlyRadioBtn
+            // 
+            this.onlyRadioBtn.AutoSize = true;
+            this.onlyRadioBtn.Location = new System.Drawing.Point(12, 255);
+            this.onlyRadioBtn.Name = "onlyRadioBtn";
+            this.onlyRadioBtn.Size = new System.Drawing.Size(190, 17);
+            this.onlyRadioBtn.TabIndex = 12;
+            this.onlyRadioBtn.Text = "Only the files in the parent directory";
+            this.onlyRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // progressTxtBox
+            // 
+            this.progressTxtBox.BackColor = System.Drawing.Color.White;
+            this.progressTxtBox.Location = new System.Drawing.Point(399, 31);
+            this.progressTxtBox.Name = "progressTxtBox";
+            this.progressTxtBox.ReadOnly = true;
+            this.progressTxtBox.Size = new System.Drawing.Size(463, 343);
+            this.progressTxtBox.TabIndex = 13;
+            this.progressTxtBox.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(480, 9);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(382, 17);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // PrimeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 386);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.progressTxtBox);
+            this.Controls.Add(this.onlyRadioBtn);
+            this.Controls.Add(this.allRadioBtn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pathTxtBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.replaceTextTxtBox);
+            this.Controls.Add(this.findTextTxtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExecute);
+            this.Name = "PrimeForm";
+            this.Text = "Word External FindAndReplace Soft - (c) SmashHeaders Ind.";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.PrimeForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.PrimeForm_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox findTextTxtBox;
+        private System.Windows.Forms.TextBox replaceTextTxtBox;
+        private System.Windows.Forms.TextBox pathTxtBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton allRadioBtn;
+        private System.Windows.Forms.RadioButton onlyRadioBtn;
+        private System.Windows.Forms.RichTextBox progressTxtBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Word/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Word;
+using System.Diagnostics;
+
+namespace Word
+{
+    public partial class PrimeForm : Form
+    {
+        public PrimeForm()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(PrimeForm_DragEnter);
+            this.DragDrop += new DragEventHandler(PrimeForm_DragDrop);
+        }
+
+        IEnumerable<FileInfo> fileList;
+        string buffer;
+        int filesOpened = 0, nrDocs = 0, oldValue = 0, intervalProgressBar = 15;
+
+        private void findAndReplace(Microsoft.Office.Interop.Word.Application wordApp, object findText, object replaceText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object matchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+
+            wordApp.Selection.Find.Execute(ref findText,
+                ref matchCase, ref matchWholeWord,
+                ref matchWildCards, ref matchSoundsLike,
+                ref matchAllWordForms, ref forward,
+                ref wrap, ref format, ref replaceText,
+                ref replace, ref matchKashida,
+                ref matchDiacritics, ref matchAlefHamza,
+                ref matchControl);
+        }
+
+        private void changeWordDocument(object fileName, object findText, object replaceText)
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("WINWORD"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problems at closing processes");
+            }
+
+            object missing = System.Reflection.Missing.Value;
+            Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+            Microsoft.Office.Interop.Word.Document aDoc = null;
+            object doNotSaveChanges = Microsoft.Office.Interop.Word.WdSaveOptions.wdDoNotSaveChanges;
+
+            buffer += "Opening file: " + Environment.NewLine + (string)fileName + Environment.NewLine;
+
+            if (File.Exists((string)fileName))
+            {
+                buffer += ".....File opened" + Environment.NewLine;
+
+                object readOnly = false;
+                object isVisible = false;
+
+                wordApp.Visible = false;
+
+                aDoc = wordApp.Documents.Open(ref fileName, ref missing,
+                ref readOnly, ref missing, ref missing, ref missing,
+                ref missing, ref missing, ref missing, ref missing,
+                ref missing, ref isVisible, ref missing, ref missing,
+                ref missing, ref missing);
+
+                aDoc.Activate();
+
+                findAndReplace(wordApp, findText, replaceText);
+
+                var shapes = aDoc.Shapes;
+
+                foreach (Shape shape in shapes)
+                {
+                    try
+                    {
+                        var initialText = shape.TextFrame.TextRange.Text;
+                        var resultingText = initialText.Replace((string)findText, (string)replaceText);
+                        shape.TextFrame.TextRange.Text = resultingText;
+                    }
+                    catch (Exception exp) { }
+                }
+
+            }
+            else
+            {
+                buffer += ".....File does not exist." + Environment.NewLine;
+                return;
+            }
+
+            string fullName = aDoc.FullName;
+            object fullFileName;
+
+            if (fullName.Contains((string)findText))
+            {
+                //buffer += "....." + fullName + Environment.NewLine;
+                fullName = fullName.Replace((string)findText, (string)replaceText);
+                buffer += "....." + "Name changed" + Environment.NewLine + fullName + Environment.NewLine;
+            }
+
+            fullFileName = fullName;
+
+            aDoc.SaveAs2(ref fullFileName,
+    ref missing, ref missing, ref missing, ref missing, ref missing,
+    ref missing, ref missing, ref missing, ref missing, ref missing,
+    ref missing, ref missing, ref missing, ref missing, ref missing);
+
+            aDoc.Close(ref doNotSaveChanges, ref missing, ref missing);
+
+            /*if ((string)fileName != fullName)
+            {
+                File.Delete((string)fileName);
+                buffer += ".....Deleted: " + Environment.NewLine + fileName + Environment.NewLine;
+            }*/
+
+            Marshal.ReleaseComObject(wordApp);
+
+            buffer += "....." + "Saved successfully" + Environment.NewLine + fullName + Environment.NewLine;
+        }
+
+        private void threadMethod()
+        {
+            buffer = "Process Started" + Environment.NewLine;
+
+            try
+            {
+                foreach (FileInfo file in fileList)
+                {
+                    try
+                    {
+                        if ((file.Extension == ".doc" || file.Extension == ".docx") && !(file.Name[0] == '~' && file.Name[1] == '$'))
+                        {
+                            changeWordDocument(file.FullName, findTextTxtBox.Text, replaceTextTxtBox.Text);
+                            filesOpened++;
+                            //progressTxtBox
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        buffer += ".....File is corrupted." + Environment.NewLine;
+                    }
+                }
+
+                buffer += "Process finished successfully." + Environment.NewLine;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Exception: " + exception.ToString() + "\nPlease fill all text boxes.");
+            }
+
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("WINWORD"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problems at closing processes");
+            }
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            if (pathTxtBox.Text == "" || findTextTxtBox.Text == "" || replaceTextTxtBox.Text == "")
+            {
+                progressTxtBox.Text = "Please fill all fields";
+                return;
+            }
+
+            
+
+            if (!Directory.Exists(pathTxtBox.Text))
+            {
+                progressTxtBox.Text = "Invalid path";
+                return;
+            }
+
+            DirectoryInfo dir = new DirectoryInfo(pathTxtBox.Text);            
+
+            progressTxtBox.Text = "";
+
+            if (allRadioBtn.Checked)
+                fileList = dir.GetFiles("*.*", SearchOption.AllDirectories);
+            else
+                fileList = dir.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+
+            foreach (FileInfo file in fileList)
+            {
+                if ((file.Extension == ".doc" || file.Extension == ".docx") && !(file.Name[0] == '~' && file.Name[1] == '$'))
+                {
+                    nrDocs++;
+                }
+            }
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = nrDocs * intervalProgressBar;
+
+            Thread thr = new Thread(threadMethod);
+            thr.Start();
+
+            timer1.Start();
+
+            /*progressTxtBox.Find("File is corrupted.");
+            progressTxtBox.SelectionColor = Color.Red;*/
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressTxtBox.Text += buffer;
+            buffer = "";
+            if (oldValue == filesOpened)
+                progressBar1.Increment(1);
+            else
+            {
+                oldValue = filesOpened;
+                progressBar1.Value = filesOpened * intervalProgressBar;
+            }
+
+            if (filesOpened >= nrDocs)
+            {
+                int startIndex = 0;
+                string word = "File is corrupted.";
+                while (startIndex < progressTxtBox.TextLength)
+                {
+                    int wordStartIndex = progressTxtBox.Find(word, startIndex, RichTextBoxFinds.None);
+
+                    if (wordStartIndex != -1)
+                    {
+                        progressTxtBox.SelectionStart = wordStartIndex;
+                        progressTxtBox.SelectionLength = word.Length;
+                        progressTxtBox.SelectionColor = Color.Red;
+                    }
+                    else
+                        break;
+                    startIndex += wordStartIndex + word.Length;
+                }
+
+                timer1.Stop();
+            }
+        }
+
+        private void PrimeForm_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            pathTxtBox.Text = files[0];
+        }
+
+        private void PrimeForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+    }
+}
